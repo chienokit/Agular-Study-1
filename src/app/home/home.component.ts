159bd38a0eb8e658b8e4411297cb9d191f03624e
evotek.trongnv <evotek.trongnv>
2021-11-04 19:04:56 +0700

    Events Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public name = 'Hữu Chiến';
+  public age = 28;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public resetName(): void {
+    console.log('resetName');
+    this.name = '';
   }
-
 }

0a9a3ba665f6e3d80488ff01a23d5a68db65ab86
evotek.trongnv <evotek.trongnv>
2021-11-05 18:16:12 +0700

    Casscading Combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Hữu Chiến';
   public age = 28;
+  public vnData = [{city: "Chọn thành phố"},{city: "Nam Định", districts: ["Nam Trực", "Trực Ninh", "Hải Hậu", "Mỹ Lộc", "Xuân Trường", "Giao Thủy", "Ý Yên", "Vụ Bản"]}
+  ,{city: "Thái Bình", districts: ["Quỳnh Phụ", "Đông Hưng", "Tiền Hải", "Quỳnh Côi"]}
+    , {city: "Hà Nam", districts: ["Phủ Lí", "Bình Lục", "Duy Tiên", "Kim Bảng"]}
+  ];
+  public districts: any;
 
   constructor() {}
 
@@ -16,5 +21,11 @@ export class HomeComponent implements OnInit {
   public resetName(): void {
     console.log('resetName');
     this.name = '';
+  }
+
+  public changeCity(event: any) : void {
+    const city = event.target.value;
+    const search:any = this.vnData.find(element => element.city === city);
+    this.districts = search.districts;
   }
 }
